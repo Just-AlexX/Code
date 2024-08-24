f67fd31d90ef3dbaf3135f4c7db32671abb62937
Just-AlexX <119613266+Just-AlexX@users.noreply.github.com>
2024-08-24 20:58:14 +0300

    Add files via upload

---
+++ Web/MyWebsite/scripts.js
@@ -0,0 +1,6 @@
+//const navItems = document.querySelectorAll('.nav-item');
+//window.addEventListener('scroll', () => {
+    navItems.forEach(item => item.classList.remove('active'));
+    // Logic to determine which section is in view
+    // For example, add 'active' to the corresponding nav-item
+//});
